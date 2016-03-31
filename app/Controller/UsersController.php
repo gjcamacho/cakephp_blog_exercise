f0453acc2e7a90749c6398a4943d2e36cc142a55
Gerardo Camacho <gjcamacho@hotmail.com>
2016-03-31 12:16:35 -0600

    Creacion de asociacion

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -12,6 +12,9 @@ class UsersController extends AppController {
 	public function view($id=null){
 		if($id!=null){
 			$this->User->recursive=-1;
+			$this->User->Behaviors->load('Containable');
+			$this->User->contain(array('Post'));
+			
 			$user=$this->User->find('first',
 					array("conditions"=>array("User.id"=>$id)));
 			
