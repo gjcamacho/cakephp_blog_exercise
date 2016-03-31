a4145d94738b68e8d61e80c7a55f95e11497fccb
Gerardo Camacho <gjcamacho@hotmail.com>
2016-03-31 12:34:29 -0600

    Creacion de asociacion

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,7 +31,7 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-	public function beforeSave(){
+	public function beforeFilter(){
 		$user=$this->Session->read("User");
 		
 		if($this->request->params["controller"]!="users" && $this->request->params["action"]!="login"){
